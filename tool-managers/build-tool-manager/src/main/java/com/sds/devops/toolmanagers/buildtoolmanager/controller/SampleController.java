10f991bb3566bb01cb82d002ff4a049e34c1113e
albert <amyik100@gmail.com>
2021-08-05 14:28:51 +0900

    build-tool-manager postgresql db 연결 ( for skaffold )

--- tool-managers/build-tool-manager/src/main/java/com/sds/devops/toolmanagers/buildtoolmanager/controller/SampleController.java
+++ tool-managers/build-tool-manager/src/main/java/com/sds/devops/toolmanagers/buildtoolmanager/controller/SampleController.java
@@ -1,18 +1,37 @@
 package com.sds.devops.toolmanagers.buildtoolmanager.controller;
 
 
+import com.sds.devops.toolmanagers.common.repository.ToolEntity;
+import com.sds.devops.toolmanagers.common.repository.ToolRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.util.Streamable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class SampleController {
+
+    private final ToolRepository toolRepository;
 
     @GetMapping("/sample")
     @ResponseStatus(HttpStatus.OK)
     public String sample() {
         return "sample api works! ( build-tool-manager )";
+    }
+
+    @GetMapping("/jpa-test")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ToolEntity> jpaTest() {
+
+        toolRepository.save(new ToolEntity(null, "githubName", "github", "url1"));
+        Iterable<ToolEntity> all = toolRepository.findAll();
+
+        return Streamable.of(all).toList();
     }
 
 }
