4cf09e0add0e71b14b86bfda395c4467800a311c
albert <amyik100@gmail.com>
2021-08-05 14:38:36 +0900

    code-repo-tool-manager, image-repo-tool-manager postgresql db 연결 ( for skaffold )

--- tool-managers/code-repo-tool-manager/src/main/java/com/sds/devops/toolmanagers/coderepotoolmanager/controller/SampleController.java
+++ tool-managers/code-repo-tool-manager/src/main/java/com/sds/devops/toolmanagers/coderepotoolmanager/controller/SampleController.java
@@ -1,13 +1,22 @@
 package com.sds.devops.toolmanagers.coderepotoolmanager.controller;
 
 
+import com.sds.devops.toolmanagers.common.repository.ToolEntity;
+import com.sds.devops.toolmanagers.common.repository.ToolRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.util.Streamable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 public class SampleController {
+
+    private final ToolRepository toolRepository;
 
     @GetMapping("/sample")
     @ResponseStatus(HttpStatus.OK)
@@ -15,4 +24,13 @@ public class SampleController {
         return "sample api works! ( code-repo-tool-manager )";
     }
 
+    @GetMapping("/jpa-test")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ToolEntity> jpaTest() {
+
+        toolRepository.save(new ToolEntity(null, "githubName", "github", "url1"));
+        Iterable<ToolEntity> all = toolRepository.findAll();
+
+        return Streamable.of(all).toList();
+    }
 }
