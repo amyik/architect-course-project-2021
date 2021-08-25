84dcc8ed0885eae10e40cbed6b2a6923892f8022
albert <amyik100@gmail.com>
2021-08-25 11:55:00 +0900

    istio gateway oidc 인증 성공
    - 사용자 clams/payload를 heade에 실어서 워크로드에 전송까지 가능하게

--- backend/src/main/java/com/sds/devops/backend/controller/SampleController.java
+++ backend/src/main/java/com/sds/devops/backend/controller/SampleController.java
@@ -1,11 +1,20 @@
 package com.sds.devops.backend.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class SampleController {
@@ -14,9 +23,32 @@ public class SampleController {
     private String appName;
 
     @GetMapping("/sample")
-    @ResponseStatus(HttpStatus.OK)
     public String sample() {
         return "sample api works! : " + appName;
+    }
+
+    @GetMapping("/authentication-test")
+    public String authenticationTest(HttpServletRequest request, @RequestHeader HttpHeaders headers) throws JsonProcessingException {
+
+        String payload = headers.get("x-jwt").get(0);
+        String userEmail = getUserEmail(payload);
+
+        if (StringUtils.isEmpty(userEmail)) {
+            return "istio RequestAuthentication Error, no user email value in header";
+        }
+
+        return "istio RequestAuthentication Error works! userEmail: " + userEmail;
+    }
+
+    private String getUserEmail(String payload) throws JsonProcessingException {
+
+        byte[] decode = Base64.getDecoder().decode(payload);
+        String decodedPayload = new String(decode);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> userInfoMap = (Map<String, String>)objectMapper.readValue(decodedPayload, Map.class);
+
+        return userInfoMap.get("email");
     }
 
 }
--- backend/src/test/java/com/sds/devops/backend/controller/SampleControllerTest.java
+++ backend/src/test/java/com/sds/devops/backend/controller/SampleControllerTest.java
@@ -1,5 +1,7 @@
 package com.sds.devops.backend.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Base64;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -38,5 +44,18 @@ class SampleControllerTest {
         mockMvc.perform(get("/sample"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("sample api works!"));
+    }
+
+    @Test
+    void jwt_payload_decode() throws JsonProcessingException {
+
+        String payload = "eyJleHAiOjE2Mjk4OTE0NzksImlhdCI6MTYyOTg1NTQ3OSwiYXV0aF90aW1lIjowLCJqdGkiOiI5YmQ3YTJjMS0wM2RlLTRjYzctYjk1ZC0zOTliYzA3NDAwNjgiLCJpc3MiOiJodHRwOi8vMzQuNjQuMjIwLjE2Mjo4MDgwL2F1dGgvcmVhbG1zL2Rldm9wcyIsImF1ZCI6Im15Y2xpZW50Iiwic3ViIjoiMjZlN2M1ZDEtMTczMS00NDkyLTk0YzUtM2YzZmVlYWE4YWQ4IiwidHlwIjoiSUQiLCJhenAiOiJteWNsaWVudCIsInNlc3Npb25fc3RhdGUiOiJjNTU3YzQ5Yi0wYjVkLTQ3OWYtYjUyNS02MGJkZjk1OTBhMjMiLCJhdF9oYXNoIjoiVjdNYVNPQVdqbUZvd3dISGhiWjJkQSIsImFjciI6IjEiLCJzaWQiOiJjNTU3YzQ5Yi0wYjVkLTQ3OWYtYjUyNS02MGJkZjk1OTBhMjMiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibmFtZSI6Im1pbnNlb2sgeW9vbiIsInByZWZlcnJlZF91c2VybmFtZSI6Im15dXNlciIsImdpdmVuX25hbWUiOiJtaW5zZW9rIiwiZmFtaWx5X25hbWUiOiJ5b29uIiwiZW1haWwiOiJtbS55b29uQHNhbXN1bmcuY29tIn0";
+
+        byte[] decode = Base64.getDecoder().decode(payload);
+        String decodedPayload = new String(decode);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> userInfoMap = (Map<String, String>)objectMapper.readValue(decodedPayload, Map.class);
+        assertThat(userInfoMap.get("email")).isEqualTo("mm.yoon@samsung.com");
     }
 }
